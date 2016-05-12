df662257b75244c7a114aa12ed5c83e2ee1533d2
Diego Avella <davella@localhost.HP-635>
2016-05-12 18:19:38 +0200

    Correct some models and start to add password encryption

--- server/models/student.js
+++ server/models/student.js
@@ -1,3 +1,15 @@
+var crypto = require('crypto-js')
 module.exports = function(Student) {
-
-};
+  Student.observe('before save', function cryptPassword(ctx, next){
+    ctx.instance.password = crypto.AES.encrypt("Message", ctx.instance.password);
+    next();
+  });
+/*Useless function */
+  Student.observe('access', function checkPasswd(ctx, next){
+    var encrypt = crypto.AES.encrypt("Message", ctx.instance.password);
+    if(ctx.currentInstance.password == encrypt){
+      console.log("Accept");
+      next();
+    }
+  });
+}
