eeb498b61731286e9dcd0e5ae60c6ec2ef5f0b49
Diego Avella <davella@localhost.HP-635>
2016-05-13 16:01:25 +0200

    Crypto updated and added relations

--- server/models/student.js
+++ server/models/student.js
@@ -1,15 +1,14 @@
-var crypto = require('crypto-js')
+var CryptoJS = require('crypto-js')
+
 module.exports = function(Student) {
-  Student.observe('before save', function cryptPassword(ctx, next){
-    ctx.instance.password = crypto.AES.encrypt("Message", ctx.instance.password);
+  /*Interceptor that cript password before memoization*/
+  /*instance => req*/
+  Student.observe('before save', function(ctx, next){
+    var passwd = CryptoJS.AES.encrypt("Message", ctx.instance.password); /*AES 256*/
+    ctx.instance.password = passwd;
+    console.log(ctx.instance.password);
     next();
-  });
-/*Useless function */
-  Student.observe('access', function checkPasswd(ctx, next){
-    var encrypt = crypto.AES.encrypt("Message", ctx.instance.password);
-    if(ctx.currentInstance.password == encrypt){
-      console.log("Accept");
-      next();
-    }
-  });
+  })
+
+  
 }
