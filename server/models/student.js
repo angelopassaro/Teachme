8b70e0548627334b766335afdb089964a9da0c18
RyukXD <93Ryuk@gmail.com>
2016-05-28 00:35:04 +0200

    model university name as id

--- server/models/student.js
+++ server/models/student.js
@@ -1,4 +1,27 @@
-// changed password hidden in user.json(node_module/loopback/common) 
+//added
+var loopback = require('loopback');
+// changed password hidden in user.json(node_module/loopback/common)
 module.exports = function(Student) {
-    
+
+/*
+//added
+  function getCurrentUserId() {
+    var ctx = loopback.getCurrentContext();
+    var accessToken = ctx && ctx.get('accessToken');
+    var userId = accessToken && accessToken.userId;
+    return userId;
+  }
+
+//added
+  Student.example = function(cb){
+    var userId = getCurrentUserId();
+    if(!userId){
+      // Code if valid access token not found
+    }else{
+
+    }
+  }
+
+  //add remote method  example
+*/
 }
