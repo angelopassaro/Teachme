c1f36ebb94b7b7b029190f1c4d20627ce441b401
avantipopolo <diequito93@gmail.com>
2016-07-23 17:11:54 +0200

    Start to create platform page and fix login (email only)

---
+++ client/js/controllers/platform.js
@@ -0,0 +1,4 @@
+angular.module('app')
+    .controller('PlatformCtrl', ['$scope', '$state', function(){
+      
+      }]);
--- client/js/controllers/signin.js
+++ client/js/controllers/signin.js
@@ -1,8 +1,11 @@
 angular.module('app')
     .controller('SignInCtrl', ['$scope', 'Student', '$state', function($scope, Student){
         $scope.log = function(){
-          $scope.formLogin.ttl = 60 * 60;
-          Student.login($scope.formLogin).$promise
+          Student.login({
+            email: $scope.formLogin.user,
+            password: $scope.formLogin.password,
+            ttl: 60 * 60
+            }).$promise
           .then(function(user){
             console.log(user);
           });
