26273d69f07b4870d092e823a06fff9238e05bb2
avantipopolo <diequito93@gmail.com>
2016-08-17 17:41:35 +0200

    rewrite getSkills

--- client/js/controllers/skill.js
+++ client/js/controllers/skill.js
@@ -1,7 +1,7 @@
 define(['app'], function(app){
   'use-strict';
-  app.controller('SkillCtrl', ['$scope', '$state', 'Student', 'Lesson', 'Course', 'University',
-    function($scope, $state, Student, Lesson, Course, University){
+  app.controller('SkillCtrl', ['$scope', '$state', 'Student', 'Lesson', 'Course', 'University', 'Teacher',
+    function($scope, $state, Student, Lesson, Course, University, Teacher){
       /*Initialization of scope Variables*/
       $scope.Skill = $scope.Skill || {};
       University.find(function(success){
@@ -22,46 +22,26 @@ define(['app'], function(app){
       /*Functions from API.*/
       $scope.getSkills = function(){
         var tutorSkills = [];
-        Lesson.find({filter: {where: {studentId: Student.getCurrentId()}}},
-          function(lessons){
-            for(var i=0; i<lessons.length; i++){
-              json = {};
-              fillJSON(json, lessons[i]);
-              tutorSkills[i] = json;
-            }
-            console.log(tutorSkills);
-            },
-            function(error){
-              console.log(error);
-            });
+        Lesson.find({filter: {where: {studentId: Student.getCurrentId()}}}, function(lesson){
+          lesson.map(function(value, index){
+            var json = {};
+            json.lesson = value;
+            Course.findById({id: json.lesson.courseId},function(course){
+              json.course = course;
+              Course.toughtBy({id: json.course.id}, function(teacher){
+                json.teacher = teacher;
+                Student.university({id: Student.getCurrentId()}, function(university){
+                  json.university = university;
+                }, function(error){console.log(error);});
+              }, function(error){ console.log(error)});
+            }, function(error){console.log(error);});
+            tutorSkills[index] = json;
+          });
+        }, function(error){console.log(error);});
         return tutorSkills;
       };
 
-      fillJSON = function(json, lesson){
-        json['lesson'] = lesson;
-        Course.findById({id: json.lesson.courseId},
-            function(course){
-              json['course'] = course;
-              Course.toughtBy({id: json.course.id},
-                function(teacher){
-                  json['teacher'] = teacher;
-                  Student.university({id: Student.getCurrentId()},
-                    function(university){
-                      json['university'] = university;
-                      },
-                      function(error){
-                        console.log(error);
-                      });
-                },
-                  function(error){
-                    console.log(error);
-                  });
-            },
-              function(error){
-                console.log(error);
-              });
-      };
-
+      var oldValues;
       $scope.editForm = function(index){
         for(var i=index; i<$scope.skills.length; i++){
            $scope.skills[i].visible = true;
@@ -71,27 +51,11 @@ define(['app'], function(app){
         $scope.Skill.course = $scope.skills[index].course.name;
         $scope.Skill.teacher = $scope.skills[index].teacher[0].name + ' ' + $scope.skills[index].teacher[0].lastName;
         $scope.Skill.price = $scope.skills[index].lesson.totalPrice;
+        oldValues = $scope.Skill;
       };
 
       $scope.editSkill = function(){
-        Student.findById({id: Student.getCurrentId()}, function(student){
-          University.findById({id: student.universityId}, function(university){
-            University.offers({id: university.id}, function(course){
-              Lesson.find({filter: {where: {studentId: Student.getCurrentId(), courseId: course.id}}}, function(lesson){
-                lesson.price = $scope.Skill.price;
-              }, function(error){console.log("Lesson FIND ERROR");});
-              Course.toughtBy({id: course.id}, function(teacher){
-                teacher.name = $scope.Skill.teacher.split(' ')[0];
-                teacher.lastName = $scope.Skill.teacher.split(' ')[1];
-                teacher.$save();
-              }, function(error){console.log("Course TOUGHTBY ERROR");});
-              course.name = $scope.Skill.course;
-              course.$save();
-            }, function(error){console.log("University OFFERS ERROR");});
-            university.id = $scope.Skill.university;
-            university.$save();
-          }, function(error){console.log("University FINDBYID ERROR");});
-        }, function(error){console.log("Student FINDBYID ERROR");});
+        console.log(oldValues);
       };
 
       $scope.createForm = function(){
