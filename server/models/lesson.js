3e4c6fc93036446982a021cbb3e0d5ad17f10407
RyukxD <93Ryuk@gmail.com>
2016-08-18 22:21:02 +0200

    Update lesson.js

--- server/models/lesson.js
+++ server/models/lesson.js
@@ -5,13 +5,23 @@ module.exports = function(Lesson) {
 // check the student for tutoring
     Lesson.observe('before save', function(ctx, next) {
 
+        var id;
+
         var error = new Error();
         error.status = 401;
         error.message = 'Need to be tutor';
 
+        if(ctx.isNewInstance) {
+           id = ctx.instance.studentid;
+         }
+        
+        if(ctx.data) {
+            id = ctx.data.studentid;
+        }
+
         Lesson.app.models.Student.findOne({
             where:{
-                email: ctx.instance.studentId
+                email: id
             }
         }, function(err, student) {
              student.isTutor ?  next() : next(error);
