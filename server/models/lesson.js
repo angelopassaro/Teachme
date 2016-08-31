7f0585d436114fd9e18d6a361d9d7a39ee3d0582
RyukXD <93Ryuk@gmail.com>
2016-08-31 20:17:10 +0200

    refactor lesson

--- server/models/lesson.js
+++ server/models/lesson.js
@@ -2,35 +2,37 @@ module.exports = function(Lesson) {
 
 
 
-// check the student for tutoring
+    // check the student for tutoring
     Lesson.observe('before save', function(ctx, next) {
-
-        var id = "";
-        var date = Date();
-
 
         var error = new Error();
         error.status = 401;
-        error.message = "Can't create this lesson(check the data of lesson). You need to be tutor. ";
-
 
         if(ctx.isNewInstance) {
-            
-           id = ctx.instance.studentId;
-           date = ctx.instance.dateLesson;
-       }
 
-        if(ctx.data) {
-
-            id = ctx.currentInstance.studentId;
-            date = ctx.currentInstance.dateLesson;
-    }
-
-
-        Lesson.app.models.Student.findById(id, function(err, student) {
-             student.isTutor && date > Date.now() ?  next() : next(error);
-        })
+            Lesson.app.models.Course.findById(ctx.instance.courseId, function(err, course) {
+                if(course) {
+                    course.toughtBy.count({teacherId: ctx.instance.belongsId},function(err,count) {
+                        if (count == 1) {
+                            Lesson.app.models.Student.findById(ctx.instance.studentId, function(err, student) {
+                                student.isTutor && ctx.instance.dateLesson > Date.now() ?  next() : next(error.message = "Can't create this lesson(check the data of lesson). You need to be tutor. ");
+                            })
+                        } else {
+                        next(error.message = "Don't exist a course with this teacher");
+                    }
+                    })
+                } else {
+                    next(error.message = "Course don't exist");
+                }
+            })
+        } else {
+            if(ctx.data) {
+                // if the lesson is require can't change price
+                ctx.currentInstance.available == false && ctx.data.hasOwnProperty('totalPrice') ? next(error.message = "Can't change che price now") : next ();
+            }
+        }
 
     })
 
-};
+
+}
