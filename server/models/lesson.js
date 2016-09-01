672770e647e6790b23084110f8ae0a87c62051f4
RyukXD <93Ryuk@gmail.com>
2016-09-01 22:59:51 +0200

    fix

--- server/models/lesson.js
+++ server/models/lesson.js
@@ -34,79 +34,79 @@ module.exports = function(Lesson) {
     })
 
 
-//     Lesson.search = function (token, course, teacher, cb) {
-//
-//         var error = new Error();
-//         error.status = 401;
-//
-//         var list = [];
-//
-//         Lesson.find({
-//             where:{
-//                 courseId: course,
-//                 belongId: teacher
-//             }
-//         }, function(err, lessons) {
-//             if (lessons) {
-//                 Lessons.app.models.AccessToken.findById(token, function(err, token) {
-//                     if (token) {
-//                         Lesson.app.models.Student.findById(token.userId, function(err, student) {
-//                             if (student) {
-//                                 var length = lessons.length;
-//                                 if(student.mypasspartout.expiredDate > Date.now()) {
-//
-//                                     /* Deve tornare tutte le lezioni con course e teacher lo studente di quella lezione più una media dei Feedback
-//                                     student find ->lesson[i].studentId  feed  find -> lesson[i].studentid , lesson[i].courseId TODO chiedi privacy
-//                                     JSON {lessonid :{ username : valore ,name:valore , cognome:valore , contact: valore, prezzo: valore, data:valore, durata:valore,  feed: valore} }
-//
-//                                     mydata = {}
-//                                     mydata[username] = valore
-//                                     ...
-//                                     list.push(mydata)
-//
-//
-//                                     provare una funzione (lessons, bool, cb)  richiamata function (lessons, true,function (result){  cb(result) })
-//                                     */
-//                                     cb(null,lessons);
-//
-//                                 } else {
-//
-//                                 }
-//                             } else {
-//                                 cb(error.message = "Student don't exist");
-//                             }
-//                         })
-//
-//                     } else {
-//                         cb(error.message = "Invalid session");
-//                     }
-//                 })
-//
-//
-//             } else {
-//                 cb(error.message = " No lessons found");
-//
-//             }
-//
-//         })
-//
-//     }
-//
-//
-//
-//     Lesson.remoteMethod(
-//         'search',
-//         {
-//             description: 'Find all lessons with specific course and teacher ',
-//             accepts: [
-//                 {arg: 'token', type: 'string', required: true, description: 'The token of current user'},
-//                 {arg: 'course', type: 'string', required: true, description: 'The course of lesson'},
-//                 {arg: 'teacher', type: 'string', required: true, description: ' The teacher of course'}
-//             ],
-//             returns: {arg: 'list', type: 'array'},
-//             http: {verb: 'post', path: '/search'}
-//         }
-//     )
-//
-//
-// }
+    // Lesson.search = function (token, course, teacher, cb) {
+    //
+    //     var error = new Error();
+    //     error.status = 401;
+    //
+    //     var list = [];
+    //
+    //     Lesson.find({
+    //         where:{
+    //             courseId: course,
+    //             belongId: teacher
+    //         }
+    //     }, function(err, lessons) {
+    //         if (lessons) {
+    //             Lessons.app.models.AccessToken.findById(token, function(err, token) {
+    //                 if (token) {
+    //                     Lesson.app.models.Student.findById(token.userId, function(err, student) {
+    //                         if (student) {
+    //                             var length = lessons.length;
+    //                             if(student.mypasspartout.expiredDate > Date.now()) {
+    //
+    //                                 /* Deve tornare tutte le lezioni con course e teacher lo studente di quella lezione più una media dei Feedback
+    //                                 student find ->lesson[i].studentId  feed  find -> lesson[i].studentid , lesson[i].courseId TODO chiedi privacy
+    //                                 JSON {lessonid :{ username : valore ,name:valore , cognome:valore , contact: valore, prezzo: valore, data:valore, durata:valore,  feed: valore} }
+    //
+    //                                 mydata = {}
+    //                                 mydata[username] = valore
+    //                                 ...
+    //                                 list.push(mydata)
+    //
+    //
+    //                                 provare una funzione (lessons, bool, cb)  richiamata function (lessons, true,function (result){  cb(result) })
+    //                                 */
+    //                                 cb(null,lessons);
+    //
+    //                             } else {
+    //
+    //                             }
+    //                         } else {
+    //                             cb(error.message = "Student don't exist");
+    //                         }
+    //                     })
+    //
+    //                 } else {
+    //                     cb(error.message = "Invalid session");
+    //                 }
+    //             })
+    //
+    //
+    //         } else {
+    //             cb(error.message = " No lessons found");
+    //
+    //         }
+    //
+    //     })
+    //
+    // }
+    //
+    //
+    //
+    // Lesson.remoteMethod(
+    //     'search',
+    //     {
+    //         description: 'Find all lessons with specific course and teacher ',
+    //         accepts: [
+    //             {arg: 'token', type: 'string', required: true, description: 'The token of current user'},
+    //             {arg: 'course', type: 'string', required: true, description: 'The course of lesson'},
+    //             {arg: 'teacher', type: 'string', required: true, description: ' The teacher of course'}
+    //         ],
+    //         returns: {arg: 'list', type: 'array'},
+    //         http: {verb: 'post', path: '/search'}
+    //     }
+    // )
+
+
+}
