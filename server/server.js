250de74ff0f168a6225176dd9b8c861d4d21ca64
RyukxD <93Ryuk@gmail.com>
2016-09-06 03:53:58 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.start = function() {
         var baseUrl = app.get('url').replace(/\/$/, '');
         console.log('Web server listening at: %s', baseUrl);
         console.log(app.get('url'))
-        console.log(app,get('port'))
+        console.log(app.get('port'))
         if (app.get('loopback-component-explorer')) {
             var explorerPath = app.get('loopback-component-explorer').mountPath;
             console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
